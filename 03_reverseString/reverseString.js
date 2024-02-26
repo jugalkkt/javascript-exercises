48f9de1ca193b60aa2488ed808d5a83af7c3de7b
jugalkkt <jugalkakkat@gmail.com>
2024-02-26 18:03:31 +0530

    Fix issue in reverseString.js and pass all testcases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = "";
+    for (let i = str.length - 1; i > -1; i--){
+        reverseStr += str[i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line

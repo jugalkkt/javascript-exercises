d3983e594baa200ba971844d15751456ef976ca1
jugalkkt <jugalkakkat@gmail.com>
2024-02-26 20:54:02 +0530

    Fix issue in palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+function isAlphaNum(character)
+{
+    return (/[a-zA-Z]/.test(character) || /[0-9]/.test(character));
+}
+const palindromes = function (string) {
+    let pureString = "";
+    for (let character of string){
+        if (isAlphaNum(character) && !/[0-9]/.test(character)) character = character.toLowerCase();
+        if (isAlphaNum(character)) pureString += character;
+    } 
+    let reversePureString = ""; 
+    for (let i = pureString.length - 1; i > -1; i--) reversePureString += pureString[i];
+    return (pureString == reversePureString);
 };
 
 // Do not edit below this line

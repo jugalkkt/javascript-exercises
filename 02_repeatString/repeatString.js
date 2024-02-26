ada461edd1a64ab1903e5a6d80e04ee306139632
jugalkkt <jugalkakkat@gmail.com>
2024-02-26 17:54:13 +0530

    Fix issue in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    let temp = str;
+    for (let i = 0; i < numTimes - 1; i++){
+        str += temp;
+    }
+    return str;
 };
 
 // Do not edit below this line

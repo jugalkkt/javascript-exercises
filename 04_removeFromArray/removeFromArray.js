46ebf6f44bb049ed5882c096654f95aeb8099a52
jugalkkt <jugalkakkat@gmail.com>
2024-02-26 18:39:48 +0530

    Fix issues in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let answer = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) answer.push(item);
+    });
+    return answer;
 };
 
 // Do not edit below this line
